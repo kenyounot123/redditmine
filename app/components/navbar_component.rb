29cda6d90735f7cbfc61e93f99b2e31f9dedf129
Ken Lu <kenlu519@gmail.com>
2025-08-17 16:58:02 -0400

    style: update landing page styling

--- app/components/navbar_component.rb
+++ app/components/navbar_component.rb
@@ -3,13 +3,19 @@
 class NavbarComponent < ViewComponent::Base
   include ApplicationHelper
 
-  def initialize
+  def initialize(controller:, action:)
+    @controller = controller
+    @action = action
+  end
+
+  def render?
+    controller_name == @controller && action_name == @action
   end
 
   private
 
   def navbar_hero_logo
-    image_tag("icon.svg", class: "w-6 h-6")
+    image_tag("icon.svg", class: "w-6 h-6", alt: "RedditMine")
   end
 
   def sign_in_button
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
   def primary_button_class
-    "font-semibold py-1 px-4 bg-orange-500 rounded-md text-white"
+    "font-semibold py-1 px-4 bg-orange-500 rounded-sm text-white cursor-pointer"
+  end
+
+  def secondary_button_class
+    "font-semibold py-1 px-4 rounded-sm text-white border border-1 border-orange-500 cursor-pointer"
   end
 end
