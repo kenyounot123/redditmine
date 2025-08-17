86d986379f6f68ecc449549d58f59c6169837661
Ken Lu <kenlu519@gmail.com>
2025-08-17 00:10:06 -0400

    style: update landing page and update navbar component

--- app/components/navbar_component.rb
+++ app/components/navbar_component.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class NavbarComponent < ViewComponent::Base
+  include ApplicationHelper
+
   def initialize
   end
 
@@ -8,5 +10,9 @@ class NavbarComponent < ViewComponent::Base
 
   def navbar_hero_logo
     image_tag("icon.svg", class: "w-6 h-6")
+  end
+
+  def sign_in_button
+    link_to "Sign In", "#", class: primary_button_class
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def primary_button_class
+    "font-semibold py-1 px-4 bg-orange-500 rounded-md text-white"
+  end
 end
