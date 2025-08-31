6a354a57d5a45d39b9fc6f05bdce566b74767578
Ken Lu <kenlu519@gmail.com>
2025-08-31 17:16:36 -0400

    feat: create subreddit search view component

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :subreddits
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,5 +10,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
+  resources :subreddits
+  resource :subreddit_search, only: [ :show ]
   root "landings#welcome"
 end
